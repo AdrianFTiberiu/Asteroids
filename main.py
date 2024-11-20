82d7588dcb6336f010cb0aba0f37d10fcbfb6535
AdrianFTiberiu <adrianfarcastiberiu@gmail.com>
2024-11-20 21:47:12 +0200

    player character triangle sprite coded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import * 
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -7,12 +8,25 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    Player.containers = (drawable, updatable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        #player.update(dt)
+        for object in updatable:
+            object.update(dt)
+        screen.fill((0, 0, 0)) #color black
+        #player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
